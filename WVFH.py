212e92da2200d2e3c9ac9778767f240a11f8912f
Jan Bezler <jan.filip.bezler@gmail.com>
2021-07-13 17:11:44 +0200

    Add recognising items form image

--- WVFH.py
+++ WVFH.py
@@ -1,5 +1,6 @@
 import requests
 from math import sqrt
+import pytesseract
 
 
 def possible_sell_price(name: str):
@@ -24,3 +25,27 @@ def possible_sell_price(name: str):
         sum([(mean - price)**2 for price in prices]) / (amount-1))
 
     return round(mean - std/2, 1)
+
+
+def get_items(all_items: list):
+
+    readed_line = ""
+    recognized_items = list()
+
+    pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract'
+    for line in pytesseract.image_to_string(r'vf3.png').split("\n"):
+        if "Prime" in line:
+            readed_line = line
+
+    for item in all_items:
+        if item in readed_line:
+            recognized_items.append(item)
+
+    return recognized_items
+
+
+all_prime_items = [item["item_name"] for item in requests.get(
+    f"https://api.warframe.market/v1/items").json()["payload"]["items"] if "_prime_" in item["url_name"]]
+
+for item in get_items(all_prime_items):
+    print(item, possible_sell_price(item))
